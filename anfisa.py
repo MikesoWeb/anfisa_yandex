b816d50c1f19d7fd303e56781195989a827d925b
MikesoWeb <klionpsyx@gmail.com>
2021-01-12 17:27:04 +0300

    'add_anfisa'

---
+++ anfisa.py
@@ -0,0 +1,68 @@
+# подключите библиотеку datetime под именем dt
+import datetime as dt
+from config_anfisa import DATABASE, UTC_OFFSET, queries
+
+
+def format_count_friends(count_friends):
+    if count_friends == 1:
+        return '1 друг'
+    elif 2 <= count_friends <= 4:
+        return f'{count_friends} друга'
+    else:
+        return f'{count_friends} друзей'
+
+
+def what_time(city):
+    offset = UTC_OFFSET[city]
+    city_time = dt.datetime.utcnow() + dt.timedelta(hours=offset)
+    f_time = city_time.strftime("%H:%M")
+    return f_time
+
+
+def process_anfisa(query):
+    if query == 'сколько у меня друзей?':
+        count_string = format_count_friends(len(DATABASE))
+        return f'У тебя {count_string}'
+    elif query == 'кто все мои друзья?':
+        friends_string = ', '.join(DATABASE.keys())
+        return f'Твои друзья: {friends_string}'
+    elif query == 'где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        cities_string = ', '.join(unique_cities)
+        return f'Твои друзья в городах: {cities_string}'
+    else:
+        return '<неизвестный запрос>'
+
+
+def process_friend(name, query):
+    if name in DATABASE:
+        city = DATABASE[name]
+        if query == 'ты где?':
+            return f'{name} в городе {city}'
+        elif query == 'который час?':
+            if city in UTC_OFFSET:
+                return f'В городе {city} сейчас {what_time(city)}'
+            else:
+                return f'<не могу определить время в городе {DATABASE[name]}>'
+        else:
+            return '<неизвестный запрос>'
+    else:
+        return f'У тебя нет друга по имени {name}'
+
+
+def process_query(query):
+    tokens = query.split(', ')
+    name = tokens[0]
+    if name == 'Анфиса':
+        return process_anfisa(tokens[1])
+    else:
+        return process_friend(name, tokens[1])
+
+
+def runner():
+
+    for query in queries:
+        print(query, '-', process_query(query))
+
+
+runner()
